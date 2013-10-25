b0c943f21886e6a399c2c1eaa02d2c32d3e05233
Aaron_Zhang <cs61c-sj@hive1.CS.Berkeley.EDU>
2013-10-24 22:27:32 -0700

    Fixed one bug -- incorrectly calculating padded size.

--- part1.c
+++ part1.c
@@ -24,13 +24,12 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 
 
     //Determine padded matrix size: 
-    int padded_matrix_size = (data_size_X + kern_cent_X) * (data_size_Y + kern_cent_Y);
+    int padded_matrix_size = (data_size_X + 2 * kern_cent_X) * (data_size_Y + 2 * kern_cent_Y);
 
-    //Create a padded matrix: for simplicity's sake we're just iterating through the entire thing and zeroing everything. 
-    //Optimize later?
+    //Pad the top kern y + first row's kernx
     float padded_in[padded_matrix_size];
     __m128 zero_pad = _mm_setzero_ps();  //128 bit value with all zeros. 
-    for (int i = 0; i < padded_matrix_size; i += 16) {
+    for (int i = 0; i < kern_cent_Y + kern_cent_X; i += 16) {
     	*(__m128*)(padded_in + i + 0) = zero_pad;
     	*(__m128*)(padded_in + i + 4) = zero_pad;
     	*(__m128*)(padded_in + i + 8) = zero_pad;
@@ -38,9 +37,10 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 
     }
     //Pad tail
-    for (int i = padded_matrix_size / 16 * 16; i < padded_matrix_size;  i ++) {
+    for (int i = (kern_cent_Y + kern_cent_X)/ 16 * 16; i < kern_cent_Y + kern_cent_X;  i ++) {
     	padded_in[i] = 0;
     }
+
 	__m128 array_elems_to_load0;
 	__m128 array_elems_to_load1;
 	__m128 array_elems_to_load2;
@@ -52,7 +52,7 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     		array_elems_to_load1 = _mm_load_ps (in + j * data_size_X + i + 4);
     		array_elems_to_load2 = _mm_load_ps (in + j * data_size_X + i + 8);
     		array_elems_to_load3 = _mm_load_ps (in + j * data_size_X + i + 12);
-    		//Put array elements into padded array. 
+    		//Put array elements into padded array 
     		*(__m128*)(padded_in + i + kern_cent_X + (j + kern_cent_Y) * (data_size_X + 2 * kern_cent_X) + 0) = array_elems_to_load0;
     		*(__m128*)(padded_in + i + kern_cent_X + (j + kern_cent_Y) * (data_size_X + 2 * kern_cent_X) + 4) = array_elems_to_load1;
     		*(__m128*)(padded_in + i + kern_cent_X + (j + kern_cent_Y) * (data_size_X + 2 * kern_cent_X) + 8) = array_elems_to_load2;
@@ -60,9 +60,30 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 
     	}
     	//clean-up tail for stragglers
-    	for (int j = data_size_X/16 * 16; j < data_size_X; j++) {
-    		padded_in[(i + kern_cent_Y) * (data_size_X + 2 * kern_cent_X) + kern_cent_X + j] = in[i * data_size_X + j];
+        int straggler_variable = 0;
+    	for (straggler_variable = data_size_X/16 * 16; straggler_variable < data_size_X; straggler_variable++) {
+
+    		padded_in[straggler_variable + kern_cent_X + (j + kern_cent_Y) * (data_size_X - 1 + 2 * kern_cent_X)] = in[straggler_variable + j * data_size_X];
     	}
+
+        //Padded zeros at end and front of each row.
+        for (int k = straggler_variable; k < straggler_variable + kern_cent_X * 2; k++ ) {
+            padded_in[k] = 0;
+            
+        }
+    }
+
+    for (int i = (data_size_Y + kern_cent_Y) * (data_size_X + 2 * kern_cent_X); i < padded_matrix_size ; i += 16) {
+        *(__m128*)(padded_in + i + 0) = zero_pad;
+        *(__m128*)(padded_in + i + 4) = zero_pad;
+        *(__m128*)(padded_in + i + 8) = zero_pad;
+        *(__m128*)(padded_in + i + 12) = zero_pad;
+
+
+    }
+    //Pad tail
+    for (int i = (data_size_Y + kern_cent_Y) * (data_size_X + 2 * kern_cent_X)/ 16 * 16; i < padded_matrix_size;  i ++) {
+        padded_in[i] = 0;
     }
 
     //Flip kernel
@@ -70,20 +91,6 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     for (int i = 0; i < 9; i++) {
         flipped_kernel[8 - i] = kernel[i];
     }
-
-
-
-    //Test
-    // for (int i = 0; i < data_size_X; i ++) {
-    // 	if (padded_in[i] != 0.0f) {
-    // 		printf("Found elem not zero at 0x%x\n", i );
-    // 	}
-    // 	if (i % 10000 == 0) {
-    // 		printf("Success!");
-    // 	}
-    // }
-
-    
     // main convolution loop
 	for(int y = 0; y < data_size_Y; y++){ // the x coordinate of the output location we're focusing on
         for(int x = 0; x < data_size_X; x++){ // the y coordinate of theoutput location we're focusing on
@@ -91,7 +98,7 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
                 for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped y coordinate
                     // only do the operation if not out of bounds
                     if(x + i >-1 && x + i < data_size_X && y+j>-1 && y+j<data_size_Y){
-                        //Note that thei) kernel is flipped
+                        //Using a flipped kernel for simplicity. 
                         out[x+y*data_size_X] += flipped_kernel[(kern_cent_X + i)+(kern_cent_Y + j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
                     }
                 }
